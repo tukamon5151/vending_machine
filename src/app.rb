063c7f9ef71b4d7751897ba1cef855de94b385f1
YudaiTsukamoto <yudai.sr400@gmail.com>
2020-03-04 21:59:30 +0900

    Step1

--- src/app.rb
+++ src/app.rb
@@ -3,11 +3,14 @@ require '../src/vending_machine'
 vending_machine = VendingMachine.new
 
 vending_machine.add_money(10)
-puts vending_machine.amount
+puts vending_machine.input_amount
 vending_machine.add_money(10)
-puts vending_machine.amount
+puts vending_machine.input_amount
 vending_machine.add_money(50)
-puts vending_machine.amount
+puts vending_machine.input_amount
 
 puts vending_machine.return_money
-puts vending_machine.amount
+puts vending_machine.input_amount
+
+puts vending_machine.add_money(10000)
+puts vending_machine.input_amount
--- src/vending_machine.rb
+++ src/vending_machine.rb
@@ -1,17 +1,25 @@
 class VendingMachine
-  attr_reader :total_money
+  USABLE_MONEY = [10, 50, 100, 500, 1000].freeze
+  attr_reader :input_amount
 
   def initialize
-    @total_money = 0
+    @input_amount = 0
   end
 
   def add_money(money)
-    @total_money += money
+    return money unless usable_money? money
+    @input_amount += money
   end
 
   def return_money
-    tmp_total_money = @total_money
-    @total_money = 0
+    tmp_total_money = @input_amount
+    @input_amount = 0
     tmp_total_money
+  end
+
+  private
+
+  def usable_money?(money)
+    USABLE_MONEY.include? money
   end
 end
