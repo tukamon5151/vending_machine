bc52d9e2c6a71884d8203598727bd97d07b86ba8
YudaiTsukamoto <yudai.sr400@gmail.com>
2020-03-04 22:39:34 +0900

    Step3

--- src/app.rb
+++ src/app.rb
@@ -16,3 +16,16 @@ puts vending_machine.add_money(10000)
 puts vending_machine.input_amount
 
 puts vending_machine.juice_stock_information
+
+puts vending_machine.buyable?
+puts vending_machine.buy
+puts vending_machine.sales_amount
+
+vending_machine.add_money(500)
+puts vending_machine.input_amount
+
+puts vending_machine.buyable?
+puts vending_machine.buy
+puts vending_machine.sales_amount
+puts vending_machine.input_amount
+puts vending_machine.return_money
--- src/vending_machine.rb
+++ src/vending_machine.rb
@@ -1,9 +1,12 @@
 class VendingMachine
   USABLE_MONEY = [10, 50, 100, 500, 1000].freeze
-  attr_reader :input_amount
+  attr_reader :input_amount, :sales_amount
+  COLA_PRICE = 120
 
   def initialize
     @input_amount = 0
+    @sales_amount = 0
+    @cola_stock = 5
   end
 
   def add_money(money)
@@ -17,15 +20,34 @@ class VendingMachine
     tmp_total_money
   end
 
+  def buy
+    return unless buyable?
+    @cola_stock -= 1
+    @sales_amount += COLA_PRICE
+    @input_amount -= COLA_PRICE
+  end
+
+  def buyable?
+    stock_available? && has_enough_money?
+  end
+
   def juice_stock_information
     {
       name: 'コーラ',
-      price: 120,
-      stock: 5
+      price: COLA_PRICE,
+      stock: @cola_stock
     }
   end
 
   private
+
+  def stock_available?
+    @cola_stock.positive?
+  end
+
+  def has_enough_money?
+    COLA_PRICE < @input_amount
+  end
 
   def usable_money?(money)
     USABLE_MONEY.include? money
